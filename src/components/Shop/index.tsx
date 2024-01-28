f67ecbcb291cfc9bf08bdb298590370604b958b2
MediMess <medimessarat@gmail.com>
2024-01-28 18:23:49 +0100

    Implement the Shop Section

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -3,7 +3,6 @@ import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import {
   Box,
-  Container,
   IconButton,
   Typography,
   useMediaQuery,
@@ -22,71 +21,91 @@ const Shop = () => {
 
   // Array of image URLs
   const imageUrls = [
-    "https://placekitten.com/600/300",
-    "https://placekitten.com/601/300",
-    "https://placekitten.com/602/300",
-    "https://placekitten.com/603/300",
+    "/assets/shop/Image-0.png",
+    "/assets/shop/Image-1.png",
+    "/assets/shop/Image-2.png",
+    "/assets/shop/Image-3.png",
+    "/assets/shop/Image-4.png",
   ];
 
   return (
-    <Container maxWidth="md">
-      <Box mt={4} position="relative">
-        <Typography variant="h4" gutterBottom>
-          Shop
-        </Typography>
-        <Typography variant="body1">
-          Your text goes here. Customize as needed.
-        </Typography>
+    <Box
+      position="relative"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: { xs: "20px", lg: "32px" },
+        padding: { xs: "0 24px", lg: "0 64px" },
+        maxWidth: "1440px",
+        margin: "0 auto",
+      }}
+    >
+      <Typography variant="h4">Flower Wonderland</Typography>
+      <Typography variant="body1">
+        Step into a fragrant world of floral beauty.
+      </Typography>
 
-        {/* Swiper component */}
-        <Swiper
-          modules={[Pagination, Navigation]}
-          spaceBetween={10}
-          slidesPerView={isMobile ? 1 : 3}
-          loop
-          navigation={{
-            nextEl: ".shop-swiper-button-next",
-            prevEl: ".shop-swiper-button-prev",
-          }}
-          pagination={{
-            clickable: true,
-            el: ".shop-swiper-pagination",
-          }}
-        >
-          {/* Map through the image URLs and create SwiperSlides */}
-          {imageUrls.map((imageUrl, index) => (
-            <SwiperSlide key={index}>
-              <img
-                src={imageUrl}
-                alt={`Slide ${index + 1}`}
-                style={{ width: "100%" }}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        {/* Block with Pagination and Navigation */}
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          width="100%"
-        >
-          {/* Pagination */}
-          <Box>
-            <div className="shop-swiper-pagination"></div>
-          </Box>
-          {/* Custom navigation buttons */}
-          <Box>
-            <IconButton className="cuasdfstom-swiper-button-prev">
-              <NavigateBeforeIcon />
-            </IconButton>
-            <IconButton className="shop-swiper-button-next">
-              <NavigateNextIcon />
-            </IconButton>
-          </Box>
+      {/* Swiper component */}
+      <Swiper
+        modules={[Pagination, Navigation]}
+        spaceBetween={32}
+        slidesPerView={isMobile ? 1 : 3}
+        loop
+        navigation={{
+          nextEl: ".shop-swiper-button-next",
+          prevEl: ".shop-swiper-button-prev",
+        }}
+        pagination={{
+          clickable: true,
+          el: ".shop-swiper-pagination",
+        }}
+        style={{
+          width: "100%",
+        }}
+      >
+        {/* Map through the image URLs and create SwiperSlides */}
+        {imageUrls.map((imageUrl) => (
+          <SwiperSlide
+            key={imageUrl}
+            style={{
+              width: "100%",
+            }}
+          >
+            <Box
+              sx={{
+                backgroundImage: `url(${imageUrl})`,
+                height: { xs: "300px", lg: "400px" },
+                backgroundSize: "cover",
+                borderRadius: "20px",
+                backgroundPosition: "center center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></Box>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* Block with Pagination and Navigation */}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        width="100%"
+      >
+        {/* Pagination */}
+        <Box>
+          <div className="shop-swiper-pagination"></div>
+        </Box>
+        {/* Custom navigation buttons */}
+        <Box>
+          <IconButton className="shop-swiper-button-prev">
+            <NavigateBeforeIcon />
+          </IconButton>
+          <IconButton className="shop-swiper-button-next">
+            <NavigateNextIcon />
+          </IconButton>
         </Box>
       </Box>
-    </Container>
+    </Box>
   );
 };
 
